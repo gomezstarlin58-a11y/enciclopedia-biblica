0e7afa5c791996e19628420df5426bf65da93be7
gomezstarlin58-a11y <gomezstarlin58@gmail.com>
2026-04-03 16:20:35 -0400

    Arreglo final)

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Salta los errores de TypeScript al subir a Vercel
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Salta los errores de estilo al subir a Vercel
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
